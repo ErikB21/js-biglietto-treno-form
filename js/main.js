3da6c019655d21e2a698943a47d1500df7732228
ErikBorgogno <erikborgo.20@gmail.com>
2022-07-05 17:31:22 +0200

    finit MILESTONE1

--- js/main.js
+++ js/main.js
@@ -1,33 +1,53 @@
-/*const numeroKilometri = parseInt(prompt('Quanti Km desidera fare?'));
-const prezzoKilometro = 0.21;
-const prezzoTotale = numeroKilometri * prezzoKilometro;
-
-const scontoRagazzi = (prezzoTotale * 0.8);
-const scontoAnziani = (prezzoTotale * 0.6);
-
-let n = scontoRagazzi.toFixed(2);
-let nu = scontoAnziani.toFixed(2);
-let num = prezzoTotale.toFixed(2);
 
 
+const generaBigliettoDom = document.getElementById('generaBiglietto')
+generaBigliettoDom.addEventListener('click', 
+    function(){
+        const numeroDom = document.getElementById('numero');
+        const numeroEtaDom = document.getElementById('numeroEta');
+        const prezzoKilometro = 0.21;
+        const prezzoTotale = numeroDom.value * prezzoKilometro;
 
-if (numeroKilometri >= 1){
-    const eta = parseInt(prompt('La sua Età'));
-    if (eta < 18){
-        scontoRagazzi;
-        console.log(scontoRagazzi);
-        document.getElementById('totTravel').innerHTML = 'Il prezzo totale è ' + '' + n + '€';
-    } else if (eta >= 65){
-        scontoAnziani;
-        console.log(scontoRagazzi);
-        document.getElementById('totTravel').innerHTML = 'Il prezzo totale è ' + '' + nu + '€';
-    }else{
-        prezzoTotale;
-        console.log(prezzoTotale);
-        document.getElementById('totTravel').innerHTML = 'Il prezzo totale è ' + '' + num + '€';
+        const scontoRagazzi = (prezzoTotale * 0.8);
+        const scontoAnziani = (prezzoTotale * 0.6);
+
+        let n = scontoRagazzi.toFixed(2);
+        let nu = scontoAnziani.toFixed(2);
+        let num = prezzoTotale.toFixed(2);
+
+
+
+        if (numeroDom.value >= 1){
+            if (numeroEtaDom.value < 18){
+                scontoRagazzi;
+                console.log(scontoRagazzi);
+                document.getElementById('totTravel').innerHTML = 'Il prezzo totale è ' + '' + n + '€';
+            } else if (numeroEtaDom.value >= 65){
+                scontoAnziani;
+                console.log(scontoRagazzi);
+                document.getElementById('totTravel').innerHTML = 'Il prezzo totale è ' + '' + nu + '€';
+            } else{
+                prezzoTotale;
+                console.log(prezzoTotale);
+                document.getElementById('totTravel').innerHTML = 'Il prezzo totale è ' + '' + num + '€';
+            }
+        } else{
+            console.log(numeroDom.value);
+        }
     }
-} else{
-    alert('ATTENZIONE! Inserire un numero valido!')
-    numeroKilometri;
-}*/
+    
+);
+
+const eliminaBigliettoDom = document.getElementById("eliminaBiglietto");
+eliminaBigliettoDom.addEventListener('click', 
+    function(){
+        const numeroDom = document.getElementById('numero');
+        const numeroEtaDom = document.getElementById('numeroEta');
+        numeroDom.value = '';
+        numeroEtaDom.value = '';
+        document.getElementById('totTravel').innerHTML = '';
+    }
+);
+
+
 
