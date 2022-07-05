796368352e98c7ca13c7c26bc9a61040a76a1d71
ErikBorgogno <erikborgo.20@gmail.com>
2022-07-05 14:59:19 +0200

    init project

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/*const numeroKilometri = parseInt(prompt('Quanti Km desidera fare?'));
+const prezzoKilometro = 0.21;
+const prezzoTotale = numeroKilometri * prezzoKilometro;
+
+const scontoRagazzi = (prezzoTotale * 0.8);
+const scontoAnziani = (prezzoTotale * 0.6);
+
+let n = scontoRagazzi.toFixed(2);
+let nu = scontoAnziani.toFixed(2);
+let num = prezzoTotale.toFixed(2);
+
+
+
+if (numeroKilometri >= 1){
+    const eta = parseInt(prompt('La sua Età'));
+    if (eta < 18){
+        scontoRagazzi;
+        console.log(scontoRagazzi);
+        document.getElementById('totTravel').innerHTML = 'Il prezzo totale è ' + '' + n + '€';
+    } else if (eta >= 65){
+        scontoAnziani;
+        console.log(scontoRagazzi);
+        document.getElementById('totTravel').innerHTML = 'Il prezzo totale è ' + '' + nu + '€';
+    }else{
+        prezzoTotale;
+        console.log(prezzoTotale);
+        document.getElementById('totTravel').innerHTML = 'Il prezzo totale è ' + '' + num + '€';
+    }
+} else{
+    alert('ATTENZIONE! Inserire un numero valido!')
+    numeroKilometri;
+}*/
+
